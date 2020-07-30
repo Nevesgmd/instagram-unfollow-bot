12786e49a2b1d9a05b5128f032f415e8f7dc882f
nevesgmd <gmedeirosneves@gmail.com>
2020-07-30 11:50:21 -0300

    feat: setting chromedriver manager
    Using chromedriver manager to install the correct chromedriver version and now using Instagram english interface.

--- insta_bot.py
+++ insta_bot.py
@@ -1,26 +1,27 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from getpass import getpass
 
 
 class InstaBot:
     def __init__(self, username, user_pw):
-        self.__driver = webdriver.Chrome()
+        self.__driver = webdriver.Chrome(ChromeDriverManager().install())
         self.__username = username
         self.__user_password = user_pw
         self.__driver.get("https://instagram.com")
-        sleep(2)
+        sleep(3)
         self.login()
 
     def login(self):
         self.__driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(self.__username)
         self.__driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(self.__user_password)
         self.__driver.find_element_by_xpath('//button[@type="submit"]').click()
-        sleep(2)
+        sleep(5)
         self.__driver.find_element_by_class_name('cmbtv').click()
-        sleep(1)
-        self.__driver.find_element_by_xpath("//button[contains(text(), 'Agora não')]").click()
-        sleep(1)
+        sleep(3)
+        self.__driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
+        sleep(5)
 
 
 user_name = str(input('Digite seu nome de usuário, por favor:'))
