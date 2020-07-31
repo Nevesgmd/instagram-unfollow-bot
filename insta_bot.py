62ecde5b5d1c4280de09f5e434abfc3e12d00bfb
nevesgmd <gmedeirosneves@gmail.com>
2020-07-31 17:44:34 -0300

    feat: getting 'following' usernames
    Created a function get_names() that returns the usernames of the following/followers panel and printed following usernames.

--- insta_bot.py
+++ insta_bot.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
+from time import sleep
+from random import randint
 from getpass import getpass
 
 
@@ -12,25 +14,57 @@ class InstaBot:
         self.__username = username
         self.__user_password = user_pw
         self.__driver.get("https://instagram.com")
+        self.__driver.set_window_size(1200, 700)
         self.login()
 
     def login(self):
         WebDriverWait(self.__driver, 20)\
-            .until(EC.element_to_be_clickable((By.XPATH, "//input[@name=\"username\"]")))\
+            .until(ec.element_to_be_clickable((By.XPATH, "//input[@name=\"username\"]")))\
             .send_keys(self.__username)
         self.__driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(self.__user_password)
         WebDriverWait(self.__driver, 20)\
-            .until(EC.element_to_be_clickable((By.XPATH,'//button[@type="submit"]')))\
+            .until(ec.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))\
             .click()
         WebDriverWait(self.__driver, 20)\
-            .until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button')))\
+            .until(ec.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button')))\
             .click()
         WebDriverWait(self.__driver, 20) \
-            .until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div/div/div/div[3]/button[2]"))) \
+            .until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div/div/div/div[3]/button[2]"))) \
             .click()
+
+    def get_unfollowers(self):
+        WebDriverWait(self.__driver, 20)\
+            .until(ec.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/section/'
+                                                         'div[3]/div[1]/div/div[2]/div[1]/a')))\
+            .click()
+        num_following = int(WebDriverWait(self.__driver, 20)
+                            .until(ec.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]'
+                                                                         '/section/main/div/header'
+                                                                         '/section/ul/li[3]/a/span'))).text)
+        WebDriverWait(self.__driver, 20) \
+            .until(ec.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/'
+                                                         'header/section/ul/li[3]/a/span'))) \
+            .click()
+        following = self.get_names(num_following)
+        print(following)
+
+    def get_names(self, num_users):
+        scroll_box = WebDriverWait(self.__driver, 20) \
+            .until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div/div[2]')))
+        for i in range(int(num_users/9)):
+            self.__driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scroll_box)
+            sleep(randint(500, 1250)/1000)
+        print('Finished scrolling.')
+        links = scroll_box.find_elements_by_tag_name('a')
+        names = [name.text for name in links if name.text != '']
+        WebDriverWait(self.__driver, 20) \
+            .until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div/div[1]/div/div[2]/button'))) \
+            .click()
+        return names
 
 
 user_name = str(input('Digite seu nome de usuário, por favor:'))
 user_password = getpass('Digite sua senha, por favor:')
 
 insta_bot = InstaBot(user_name, user_password)
+insta_bot.get_unfollowers()
